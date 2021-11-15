7e87eb1a2368150f046dae2fd1f77cdcd96a8bb1
Michael Merickel <michael@merickel.org>
2021-11-14 18:40:57 -0600

    archive the full tweet as a string to avoid schema issues

--- sos/output_streams/gcp_firestore.py
+++ sos/output_streams/gcp_firestore.py
@@ -1,4 +1,5 @@
 from google.cloud import firestore
+import json
 
 class GCPFirestoreOutputStream:
     def __init__(self, *, collection, client=None):
@@ -22,4 +23,16 @@ class GCPFirestoreOutputStream:
         user_sn = status['user']['screen_name']
         doc_name = f'{status_id}-{user_id}-{user_sn}'
         doc = self.collection.document(doc_name)
-        doc.set(status)
+        doc.set({
+            'id': status_id,
+            'user_id': user_id,
+            'user_sn': user_sn,
+            'created_at': status.get('created_at'),
+            'text': status.get('text'),
+            'in_reply_to_status_id': status.get('in_reply_to_status_id'),
+            'in_reply_to_user_id': status.get('in_reply_to_user_id'),
+            'in_reply_to_screen_name': status.get('in_reply_to_screen_name'),
+            'quoted_status_id': status.get('quoted_status_id'),
+            'lang': status.get('lang'),
+            'json': json.dumps(status),
+        })
